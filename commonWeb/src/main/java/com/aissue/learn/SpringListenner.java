c0e8a28084c3057a40cf5de3d9162d6ed2d0f17b
mengtian <mengtian@yscredit.com>
2018-04-28 11:58:17 +0800

    Listener 的一点点学习

---
+++ commonWeb/src/main/java/com/aissue/learn/SpringListenner.java
@@ -0,0 +1,31 @@
+package com.aissue.learn;
+
+import org.springframework.web.context.support.WebApplicationContextUtils;
+
+import javax.servlet.ServletContextEvent;
+import javax.servlet.ServletContextListener;
+
+/**
+ * 测试ServletContextListener 实现spring容器启动后台程序
+ */
+public class SpringListenner implements ServletContextListener{
+    @Override
+    public void contextInitialized(ServletContextEvent servletContextEvent) {
+        System.out.println("--------------MySpringListenner is running ------------");
+        /*
+        //一个使用的例子
+        QueueHandler queueHandler = WebApplicationContextUtils.getWebApplicationContext(servletContextEvent.getServletContext()).getBean(QueueHandler.class);
+        QueueThreadObserver queueThreadObserver = WebApplicationContextUtils.getWebApplicationContext(servletContextEvent.getServletContext()).getBean(QueueThreadObserver.class);
+        System.out.println("--------监听器启动--------");
+        ZookeeperDataConsumer consumer = new ZookeeperDataConsumer(queueHandler);
+        consumer.addObserver(queueThreadObserver);
+        Thread consumerThread = new Thread(consumer);
+        consumerThread.start();
+        */
+    }
+
+    @Override
+    public void contextDestroyed(ServletContextEvent servletContextEvent) {
+        System.out.println("--------------MySpringListenner is down ------------");
+    }
+}
