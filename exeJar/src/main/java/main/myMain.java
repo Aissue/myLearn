060d6533cb96b186f18a8448aefa276de891413c
mengtian <mengtian@yscredit.com>
2018-06-05 11:29:34 +0800

    exeJar模块的加入

---
+++ exeJar/src/main/java/main/myMain.java
@@ -0,0 +1,17 @@
+package main;
+
+import java.util.concurrent.Semaphore;
+
+/**
+ * @author yscredit
+ */
+public class myMain {
+    private static Semaphore semaphore = new Semaphore(1);
+    public static void main(String[] args) throws InterruptedException {
+        semaphore.acquire();
+        System.out.println("myMain is running...");
+        System.out.println("myMain is running...");
+        System.out.println("myMain is running...");
+        semaphore.acquire();
+    }
+}
