c4c8c13d61dd5f717db9ab1775285670bc7e7b22
mengtian <mengtian@yscredit.com>
2018-11-07 17:31:12 +0800

    增加了es的相关模块---

--- elasticSearchLearn/src/main/java/coo/ElasticsearchTest.java
+++ elasticSearchLearn/src/main/java/coo/ElasticsearchTest.java
@@ -5,11 +5,14 @@ import org.elasticsearch.common.settings.Settings;
 import org.elasticsearch.common.transport.InetSocketTransportAddress;
 import org.elasticsearch.transport.client.PreBuiltTransportClient;
 import org.junit.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import java.net.InetAddress;
 import java.net.UnknownHostException;
 
 public class ElasticsearchTest {
+    public final static Logger logger = LoggerFactory.getLogger(ElasticsearchTest.class);
 
     public final static String HOST = "192.168.248.130";
     public final static int PORT = 9300;
@@ -21,6 +24,7 @@ public class ElasticsearchTest {
         );
 
         System.out.println("Elasticsearch connect info:" + client.toString());
+        logger.info("Elasticsearch connect info ---->:" + client.toString());
         client.close();
     }
 }
