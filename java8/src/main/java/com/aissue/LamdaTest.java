7071b07776e9b2426098e034726398cf169b81c8
Administrator <aissue@126.com>
2018-12-09 23:16:26 +0800

    java8新特性

---
+++ java8/src/main/java/com/aissue/LamdaTest.java
@@ -0,0 +1,29 @@
+package com.aissue;
+
+import org.junit.Test;
+
+import java.util.Arrays;
+import java.util.List;
+
+/**
+ * Created by Administrator on 2018/12/9.
+ */
+public class LamdaTest {
+    @Test
+    public void test1(){
+        List<Dish> menu = Arrays.asList(
+                new Dish("pork", false, 800, Dish.Type.MEAT),
+                new Dish("beef", false, 700, Dish.Type.MEAT),
+                new Dish("chicken", false, 400, Dish.Type.MEAT),
+                new Dish("french fries", true, 530, Dish.Type.OTHER),
+                new Dish("rice", true, 350, Dish.Type.OTHER),
+                new Dish("season fruit", true, 120, Dish.Type.OTHER),
+                new Dish("pizza", true, 550, Dish.Type.OTHER),
+                new Dish("prawns", false, 300, Dish.Type.FISH),
+                new Dish("salmon", false, 450, Dish.Type.FISH) );
+        menu.forEach(dish -> {
+          dish.setCalories(dish.getCalories()+30);
+        });
+        menu.forEach(dish -> System.out.println(dish.getCalories()));
+    }
+}
---
+++ java8/src/main/java/com/aissue/StreamTest.java
@@ -0,0 +1,33 @@
+package com.aissue;
+
+import org.junit.Test;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
+/**
+ * Created by Administrator on 2018/12/9.
+ */
+public class StreamTest {
+    @Test
+    public void test1(){
+        List<Dish> menu = Arrays.asList(
+                new Dish("pork", false, 800, Dish.Type.MEAT),
+                new Dish("beef", false, 700, Dish.Type.MEAT),
+                new Dish("chicken", false, 400, Dish.Type.MEAT),
+                new Dish("french fries", true, 530, Dish.Type.OTHER),
+                new Dish("rice", true, 350, Dish.Type.OTHER),
+                new Dish("season fruit", true, 120, Dish.Type.OTHER),
+                new Dish("pizza", true, 550, Dish.Type.OTHER),
+                new Dish("prawns", false, 300, Dish.Type.FISH),
+                new Dish("salmon", false, 450, Dish.Type.FISH) );
+
+        List<String> three = menu.stream()
+                .filter(d -> d.getCalories()>300)
+                .map(Dish::getName)
+                .limit(3)
+                .collect(Collectors.toList());
+        System.out.println(three);
+    }
+}
